90b19b8f6632ec777c5dc60c60bc45c79435a43a
Alex Forrence <alex.forrence@gmail.com>
2020-07-11 22:59:00 -0400

    cleanup

--- info/main.js
+++ info/main.js
@@ -8,12 +8,8 @@ console.log(JSON.stringify(parser.getResult()));
 
 var config = {
     type: Phaser.WEBGL,
-    width: screen.width,// * window.devicePixelRatio,
-    height: screen.height,// * window.devicePixelRatio,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
-    },
+    width: 400,// * window.devicePixelRatio,
+    height: 400,// * window.devicePixelRatio,
     autoRound: false,
     backgroundColor: '#2d2d2d',
     parent: 'phaser-example',
@@ -31,23 +27,11 @@ var game = new Phaser.Game(config);
 
 function create ()
 {
-    this.cameras.main.setBounds(-screen.width / 2, -screen.height / 2, screen.width, screen.height);
+    this.cameras.main.setBounds(-config.width / 2, -config.height / 2, config.width, config.height);
     graphics = this.add.graphics();
-    var r1 = this.add.circle(-100, -100, 80, 0x6666ff);
-    r1.setInteractive();
-    r1.on('pointerup', function () {
-        if (this.scale.isFullscreen) { this.scale.stopFullscreen(); }
-        else { this.scale.startFullscreen(); }
-
-    }, this);
     follower = { t: 0, vec: new Phaser.Math.Vector2() };
-    path = new Phaser.Curves.Path(50, 500);
-    path.splineTo([ 164, 446, 274, 542, 412, 457, 522, 541, 664, 464 ]);
-    path.lineTo(700, 300);
-    path.lineTo(600, 350);
-    path.ellipseTo(200, 100, 100, 250, false, 0);
-    path.cubicBezierTo(222, 144, 308, 107, 208, 368);
-    path.ellipseTo(60, 60, 0, 360, true);
+    path = new Phaser.Curves.Path(100, 0);
+    path.circleTo(100, true);
 
     this.tweens.add({
         targets: follower,
