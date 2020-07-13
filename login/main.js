e3fa4c276fbba2374487be91a07b3a3be3cad463
Alex Forrence <alex.forrence@gmail.com>
2020-07-13 17:50:40 -0400

    upgrade phaser, more elaborate survey (but not quite there yet)

--- login/main.js
+++ login/main.js
@@ -23,18 +23,44 @@ function preload ()
 
 function create()
 {
-    var text = this.add.text(10, 10, 'Name will show here.', { color: 'white', fontFamily: 'Arial', fontSize: '32px'});
+    var text = this.add.text(10, 10, 'Age will show here.', { color: 'white', fontFamily: 'Arial', fontSize: '32px'});
     var element = this.add.dom(400, 300).createFromCache('form');
-    element.setPerspective(800);
     element.addListener('click');
 
     element.on('click', function (event) {
-
-        if (event.target.name === 'loginButton') {
-            var username = this.getChildByName('username');
-            if (username.value !== '') {
-                text.setText(username.value);
+        // this probably isn't the ideal way to get info,
+        // and note the validation is currently nonexistant!
+        if (event.target.name === 'submit') {
+            var radios = document.getElementsByName('sex');
+            for (var i = 0, len = radios.length; i < len; i++) {
+                if (radios[i].checked) {
+                    sex = radios[i].value;
+                    break;
+                }
             }
+            var races = [];
+            var checks = document.getElementsByName('race');
+            for (var i = 0, len = checks.length; i < len; i++) {
+                if (checks[i].checked) {
+                    races.push(checks[i].value);
+                }
+            }
+            radios = document.getElementsByName('ethnicity');
+            for (var i = 0, len = radios.length; i < len; i++) {
+                if (radios[i].checked) {
+                    ethnicity = radios[i].value;
+                    break;
+                }
+            }
+            var info = {
+                username: this.getChildByName('username').value,
+                age: this.getChildByName('age').value,
+                sex: sex,
+                races: races,
+                ethnicity: ethnicity
+            };
+            text.setText(info.age);
+            console.log(JSON.stringify(info));
         }
     });
 }
