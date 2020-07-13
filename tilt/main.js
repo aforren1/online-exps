367fd23da1329d055ad65ccfe0f5e40b5288d701
Alex Forrence <alex.forrence@gmail.com>
2020-07-12 23:05:18 -0400

    opps

--- tilt/main.js
+++ tilt/main.js
@@ -24,11 +24,11 @@ function create() {
 
     var cursor = this.add.circle(0, 0, 17.5, 0xff00ff);
 
-    if (false) {
+    if (window.DeviceMotionEvent) {
         window.addEventListener("devicemotion", function(event) {
             // alpha: rotation around z-axis
-            cursor.x += event.acceleration.x/10;
-            cursor.y += event.acceleration.y/10;
+            cursor.x += event.acceleration.x;
+            cursor.y += event.acceleration.y;
             cursor.x = Phaser.Math.Wrap(cursor.x, -game.renderer.width/2, game.renderer.width/2);
             cursor.y = Phaser.Math.Wrap(cursor.y, -game.renderer.height/2, game.renderer.height/2);
         }, true);
